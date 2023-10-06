09cc55834ed163dabbd1cac54334b43e61f000bb
tienlx97 <tienlx97@gmail.com>
2023-10-06 17:00:12 +0700

    chore: done CcometProgressRingIndeterminate

---
+++ packages/fb/src/popover/components/comet-progress-ring-indeterminate/styles.ts
@@ -0,0 +1,49 @@
+import { makeStyles } from '@fluentui/react-components'
+
+export const useStyles = makeStyles({
+  animationFillModeAndTimingFn: {
+    animationFillMode: 'x1u6ievf',
+    animationTimingFunction: 'x1wnkzza',
+  },
+  foregroundCircle: {
+    animationDuration: 'x1c74tu6',
+    animationFillMode: 'x1u6ievf',
+    animationIterationCount: 'xa4qsjk',
+    animationTimingFunction: 'xuxiujg',
+    transformOrigin: 'x1bndym7',
+  },
+  foregroundCircle12: {
+    animationName: 'x1iqdq0d',
+  },
+  foregroundCircle16: {
+    animationName: 'x1koaglw',
+  },
+  foregroundCircle20: {
+    animationName: 'x16tkgvi',
+  },
+  foregroundCircle24: {
+    animationName: 'xiepp7r',
+  },
+  foregroundCircle32: {
+    animationName: 'x1pb3rhs',
+  },
+  foregroundCircle48: {
+    animationName: 'x1w90wak',
+  },
+  foregroundCircle60: {
+    animationName: 'x1jrcm85',
+  },
+  foregroundCircle72: {
+    animationName: 'xnw30k',
+  },
+  root: {
+    display: 'x78zum5',
+  },
+  rotationCircle: {
+    animationDuration: 'x1c74tu6',
+    animationIterationCount: 'xa4qsjk',
+    animationName: 'x1kfoseq',
+    animationTimingFunction: 'x193epu2',
+    transformOrigin: 'x1bndym7',
+  },
+})
